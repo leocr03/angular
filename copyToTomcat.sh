90e1b515af4d6a195d88396c456a09d10050c3e4
leonardo <leocr03@gmail.com>
2016-09-08 09:13:54 -0300

    AngularJS Demo

--- copyToTomcat.sh
+++ copyToTomcat.sh
@@ -1,3 +1,5 @@
-cp -Rap angular_study.html /opt/tomcat/webapps/test/;
-cp -Rap controllers.js /opt/tomcat/webapps/test/;
+#!/usr/bin/env bash
 
+mkdir -p /opt/tomcat/webapps/test;
+cp -Rap angular_study.html /opt/tomcat/webapps/test/index.html;
+cp -Rap controllers.js myData.json /opt/tomcat/webapps/test/;
