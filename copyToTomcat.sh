92cb048f19d44f62c7f6b3ffd26cf2ef68be7497
leonardo <leocr03@gmail.com>
2016-09-08 09:13:54 -0300

    Adding collapse to start to organizing of demo

--- copyToTomcat.sh
+++ copyToTomcat.sh
@@ -1,3 +1,3 @@
-cp -Rap angular_study.html /opt/tomcat/webapps/test/;
-cp -Rap controllers.js /opt/tomcat/webapps/test/;
-
+#!/usr/bin/env bash
+cp -Rap angular_study.html /opt/tomcat/webapps/test/index.html;
+cp -Rap controllers.js myData.json /opt/tomcat/webapps/test/;
